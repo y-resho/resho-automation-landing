5f996ad2620d5e6de70d161423de4dab9f3d340d
Viktor Redecker <61469992+redecvi@users.noreply.github.com>
2026-01-30 12:00:46 +0100

    fix path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/resho-automation-landing/" : "/",
+export default defineConfig({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
@@ -18,4 +18,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
